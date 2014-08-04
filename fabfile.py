7b4f2d00cb029a6cbf527df36610b71c7470a2d8
Daniel Lawrence <dannyla@linux.com>
2014-08-04 19:24:39 +1000

    More pep8 fixes

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """ This is a demo fabfile that has been include to show you that the docstring
 in the fabfile file is what is shown here.
-This can be handy to give the user an overview of tasks, that are located within
-this fabfile.
+This can be handy to give the user an overview of tasks, that are located
+within this fabfile.
 """
 
 from fabric.api import task, run, settings, hide
@@ -14,6 +14,7 @@ env.roledefs = {
     'dns': ['ns1', 'ns2']
 }
 
+
 @task
 def uname(arg1, hostname='localhost', username='root'):
     """ my2 fab2 file2. """
@@ -21,14 +22,16 @@ def uname(arg1, hostname='localhost', username='root'):
         run('uname -a')
     run('uname -a')
 
+
 @task
 def get_remote_kernel():
     """ Connect to a remote system to gather its kernel version. """
     run('uname -r')
 
+
 @task
 def run_command(executable_command):
-    """ Connect to the remote system(s) are run any command that you type in. """
+    "Connect to the remote system(s) are run any command that you type in."
     run("%s" % "".join(executable_command))
 
 
@@ -38,22 +41,25 @@ def task_with_roles_deco():
     """ This has as a fabric.decorators.roles(*role_list)"""
     run('id')
 
+
 @hosts('web')
 @task
 def task_with_hosts_deco():
     """ This has as a fabric.decorators.hosts(*host_list)"""
     run('id')
 
-@hosts('localhost','localhost')
+
+@hosts('localhost', 'localhost')
 @task
 def task_with_2hosts_deco():
     """ This has as a fabric.decorators.hosts(*host_list)"""
     run('id')
 
+
 @roles('web')
 @hosts('localhost')
 @task
 def task_with_hosts_and_roles_deco():
-    """ This has as a fabric.decorators.hosts(*host_list) and fabric.decorators.roles(*role_list)"""
+    """ This has as a fabric.decorators.hosts(*host_list)
+    and fabric.decorators.roles(*role_list)"""
     run('id')
-
